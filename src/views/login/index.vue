69059262ae60fa2f2dfaae8b0126805eab511982
likai <1774574746@qq.com>
2020-11-14 16:56:30 +0800

    登录注册样式确定

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,22 +6,26 @@
       </ul>
       <!-- 登录表单 -->
       <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form demo-ruleForm" size="small">
-
-        <el-form-item prop="pass" class="item-form">
+        <el-form-item prop="username" class="item-form">
           <label>邮箱</label>
-          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
         </el-form-item>
 
-        <el-form-item prop="checkPass" class="item-form">
+        <el-form-item prop="password" class="item-form">
           <label>密码</label>
-          <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
         </el-form-item>
 
-        <el-form-item prop="age" class="item-form">
+        <el-form-item prop="passwords" class="item-form" v-if="isShow">
+          <label>重复密码</label>
+          <el-input type="password" v-model="ruleForm.passwords" autocomplete="off" minlength="6" maxlength="20"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code" class="item-form">
           <label>验证码</label>
           <el-row :gutter="11">
             <el-col :span="15">
-              <el-input v-model.number="ruleForm.age"></el-input>
+              <el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input>
             </el-col>
             <el-col :span="9">
               <el-button class="block" type="success">获取验证码</el-button>
@@ -39,74 +43,91 @@
 </template>
 
 <script>
+import { validateUser, validatepass } from '../../utils/validate'
+
 export default {
   name: 'Login',
   data () {
-    var checkAge = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error('年龄不能为空'))
-      }
-      setTimeout(() => {
-        if (!Number.isInteger(value)) {
-          callback(new Error('请输入数字值'))
-        } else {
-          if (value < 18) {
-            callback(new Error('必须年满18岁'))
-          } else {
-            callback()
-          }
-        }
-      }, 1000)
-    }
-    var validatePass = (rule, value, callback) => {
+    // 验证用户名邮箱
+    var validateUsername = (rule, value, callback) => {
       if (value === '') {
-        callback(new Error('请输入密码'))
+        callback(new Error('请输入邮箱'))
+      } else if (validateUser(value)) {
+        callback(new Error('用户名格式有误'))
       } else {
-        if (this.ruleForm.checkPass !== '') {
-          this.$refs.ruleForm.validateField('checkPass')
-        }
         callback()
       }
     }
-    var validatePass2 = (rule, value, callback) => {
+    // 验证密码
+    var validatePassword = (rule, value, callback) => {
       if (value === '') {
-        callback(new Error('请再次输入密码'))
-      } else if (value !== this.ruleForm.pass) {
-        callback(new Error('两次输入密码不一致!'))
+        callback(new Error('请输入密码'))
+      } else if (validatepass(value)) {
+        callback(new Error('密码为6~20位的字母、下划线、数字组成!'))
+      } else {
+        callback()
+      }
+    }
+    // 验证重复密码
+    var validatePasswords = (rule, value, callback) => {
+      if (value !== this.ruleForm.password) {
+        callback(new Error('两次输入的密码不同！'))
+      } else {
+        callback()
+      }
+    }
+    // 验证码
+    var checkcode = (rule, value, callback) => {
+      const reg2 = /^[a-z0-9]{6}$/
+      if (value === '') {
+        return callback(new Error('验证码不能为空'))
+      } else if (!reg2.test(value)) {
+        return callback(new Error('验证码错误！'))
       } else {
         callback()
       }
     }
     return {
       menuTab: [
-        { text: '登录', current: true },
+        { text: '登录', current: true }, // current控制登录注册按钮切换
         { text: '注册', current: false }
       ],
-      isActive: true,
+      isShow: false, // 注册中的重复密码框显示与否
       ruleForm: {
-        pass: '',
-        checkPass: '',
-        age: ''
+        username: '',
+        password: '',
+        passwords: '',
+        code: ''
       },
       rules: {
-        pass: [
-          { validator: validatePass, trigger: 'blur' }
+        username: [
+          { validator: validateUsername, trigger: 'blur' }
         ],
-        checkPass: [
-          { validator: validatePass2, trigger: 'blur' }
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
         ],
-        age: [
-          { validator: checkAge, trigger: 'blur' }
+        passwords: [
+          { validator: validatePasswords, trigger: 'blur' }
+        ],
+        code: [
+          { validator: checkcode, trigger: 'blur' }
         ]
       }
     }
   },
   methods: {
     toggleMenu (data) {
+      // 控制注册登录按钮切换时的按钮背景
       this.menuTab.forEach(el => {
         el.current = false
       })
       data.current = true
+      // 控制登录注册按钮切换时重复密码框的显示
+      if (data.text === '登录') {
+        this.isShow = false
+      } else {
+        this.isShow = true
+      }
     },
     submitForm (formName) {
       this.$refs[formName].validate((valid) => {
